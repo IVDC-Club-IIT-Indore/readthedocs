031bbe3f10b72beecbebb28ea44b0404fe243bf3
KshitijBhat <me200003042@iiti.ac.in>
2022-07-11 21:08:05 +0530

    add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,10 +33,15 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+
+#html_favicon 
+html_favicon = 'favicon.ico'
+
